082dc207379b840303172ada8f38af47b600fdca
danieldamian09 <humbertorivero9@gmail.com>
2022-01-07 13:11:00 -0300

    control del slider e indicadores

--- Slider/main.js
+++ Slider/main.js
@@ -7,25 +7,54 @@ class Slider {
     this.slider = document.querySelector(selector);
     this.interval = null;
     this.contador = 0;
+    // Seleccionamos los hijos directos del container para saber cuantas imagenes tenemos
+    this.itemsCount = this.slider.querySelectorAll(".container > *").length;
     this.start();
+
+    // Crear los controles
+    this.buildControls();
   }
 
   start() {
-    this.interval = window.setInterval(this.move, 5000)
+    this.interval = window.setInterval(this.move, 2000)
+  }
+
+  buildControls(){
+    for (let i = 0; i < this.itemsCount; i++) {
+      let control = document.createElement('li');
+
+      // Agregar la clase active al primer control
+      if (i === 0) control.classList.add('active')
+
+      this.slider.querySelector(".controls ul").appendChild(control)
+      
+    }
   }
 
   move() {
-    // Seleccionamos los hijos directos del container para saber cuantas imagenes tenemos
-    let itemsCount = document.querySelectorAll(".container > *").length;
-    // console.log(itemsCount)
     this.contador++;
     // Para resetear el contador
-    if(this.contador > itemsCount - 1) this.contador = 0;
+    if(this.contador > this.itemsCount - 1) this.contador = 0;
     this.moveTo(this.contador);
+  }
+
+  // Selecionamos todos los elementos que tiene la clase active y los iteramos
+  resetIndicador(){
+    this.slider.querySelectorAll(".controls .active")
+      .forEach(item => item.classList.remove("active"));
   }
 
   moveTo(index) {
     let left = index * 100;
+
+    // Reinicar el indicador para que uno permanezca activo
+    this.resetIndicador();
+
+
+    // Agregar la clase active el control en el cual se encuentra la imagen
+    this.slider.querySelector(`.controls li:nth-child(${index+1})`).classList.add("active");
+
+    console.log(index);
 
     this.slider.querySelector(".container").style.left = "-" + left + "%";
   }
