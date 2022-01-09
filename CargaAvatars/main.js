5a1a32fbc6f2bc0dac83b8aa36e0cb089f32c0b5
danieldamian09 <humbertorivero9@gmail.com>
2022-01-08 21:12:39 -0300

    subir imagenes de avatar en una card de perfil

---
+++ CargaAvatars/main.js
@@ -0,0 +1,15 @@
+(function(){
+  document.querySelector('#file-uploader').addEventListener('change', function(e){
+      let files = e.target.files;
+    console.log(files)
+
+    let image = files[0];
+
+    let imageURL = URL.createObjectURL(image);
+
+    document.querySelector(".profile .img").style.backgroundImage = "url('"+imageURL+"')";
+
+
+    });
+
+})();
