1ed743e54f0a481b9bf7737d7f545d3f93aaf27d
danieldamian09 <humbertorivero9@gmail.com>
2022-01-14 10:00:50 -0300

    crear metodo datalist y asignar options

--- Autocompletado/main.js
+++ Autocompletado/main.js
@@ -36,14 +36,77 @@ class Search {
 
 // Clase de Autocompletar
 class Autocomplete {
+  
+  constructor(input_selector, base_url){
+    // Creamos un metodo que nos va a crear una datalist
+    this.input = document.querySelector(input_selector);
+    this.url = base_url;
+    this.buildDataList();
+  }
+
+  
+    // Construir el node (datalist)
+    buildDataList(){
+      // creamos datalist
+      this.dataList = document.createElement("datalist");
+      // asignamos un id
+      this.dataList.id = "datalist-autocomplete";
+      // le agregamos al input el atributo "list" y lo enlazamos con el nombre que esta en el atributo ID "esto para enlazar el input con el data list"
+      this.input.setAttribute("list", "datalist-autocomplete");
+      // insetar este elemento que acabamos de crear en el body (datalist)
+      document.querySelector("body").appendChild(this.dataList);
+    }
+
+    // Construir todos los options que van dentro de datalist por medio de la respuesta que recibimos del servidor (JSON)
+    build(response){
+      // reiniciamos el datalist por si tiene opciones viejas
+      this.dataList.innerHTML = "";
+      // despues iteramos la respuesta y por cada uno creamos los options (items viene de la respuesta JSON)
+      response.items.forEach(item => {
+        //? creamos el nuevo nodo del DOM (options)
+        let optionEl = document.createElement("option");
+        // ? le asignamos a optionEl como valor las siguientes opciones
+        optionEl.value = item.volumeInfo.title;
+        // ? validamos si el item tiene un subtitulo para utilizarlo en optionEl
+        if(item.volumeInfo.title) optionEl.innerHTML = item.volumeInfo.title;
+
+        // insertamos dentro del datalist estas opciones (optionEl)
+        this.dataList.appendChild(optionEl);
+
+      });
+
+    }
+
+    search(){
+      Search.get(this.url+"harry")
+      .then(results => this.build(results));
+      this.url = "https://www.googleapis.com/books/v1/volumes?q=;"
+    }
 
 }
 
 (function () {
   const GoogleBooksApiURL = "https://www.googleapis.com/books/v1/volumes?q=;"
-  Search.get(GoogleBooksApiURL+"harry")
-    .then(results => console.log(results));
+
+  // instanciamos un nuevo objeto de la clase autocomplete
+  let automcomplete = new Autocomplete("#searcher", GoogleBooksApiURL);
+  automcomplete.search();
+
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -66,3 +129,4 @@ class Search {
 })();
 
 */
+
