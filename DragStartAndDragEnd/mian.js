dbecc39fae1b203bc81a10c377621749a1712d34
danieldamian09 <humbertorivero9@gmail.com>
2022-01-14 15:23:18 -0300

    arrastrar elemento y quitar fantasma de referencia

--- DragStartAndDragEnd/mian.js
+++ DragStartAndDragEnd/mian.js
@@ -1,9 +1,13 @@
-class DOMHelper{
+class DOMHelper {
+  static move(el, coords){
+    el.style.top = coords.y - (el.clientHeight / 2) + "px"
+    el.style.left = coords.x - (el.clientWidth / 2) + "px"
+  }
 
 }
 
-class DragList{
-  constructor(list_selector, items_selector="li"){
+class DragList {
+  constructor(list_selector, items_selector = "li") {
     // Nodo de la lista:
     this.list = document.querySelector(list_selector);
     // Todos los Nodos Hijos de la lista:
@@ -13,46 +17,52 @@ class DragList{
     this.handleDrag = this.handleDrag.bind(this);
     this.handleDragEnd = this.handleDragEnd.bind(this)
 
+    // crear el canvas vacios para mostrar cuando movemos la imagen
+    this.canvas = document.createElement("canvas");
+
     // colocar los correspodientes listener
     this.bindEvents();
   }
 
   //* colocar los correspodientes listener
-  bindEvents(){
+  bindEvents() {
     // hacemos un forEach para todos los nodos hijos de la lista(en este caso)
+    // console.log(this.items)
     this.items.forEach(item => {
-      console.log(item)
       item.addEventListener("dragstart", this.handleDragStart)
       item.addEventListener("drag", this.handleDrag)
       item.addEventListener("dragend", this.handleDragEnd)
     });
+
   }
 
   //* Metodo cuando el usuario inicie el drag and drop
-  handleDragStart(e){
+  handleDragStart(e) {
     // Obtener el elemento actual que se esta arrastrando
-    let element = e.currentTarget;
-    console.log(element)
-    console.log(":)")
-    element.classList.add("draggin");
+    let el = e.currentTarget;
+    // console.log(":)")
+    e.dataTransfer.setDragImage(this.canvas,0,0);
+    el.classList.add("draggin");
 
   }
 
   //* Metodo mientras el usuario esta arrastrando
-  handleDrag(){
-    
+  handleDrag(e) {
+    DOMHelper.move(e.currentTarget, {x: e.clientX, y: e.clientY})
   }
 
   //* Metodo cuando el usuario suelte el elemento
-  handleDragEnd(e){
-    let element = e.currentTarget;
-    console.log(":(")
-    element.classList.remove("draggin");
+  handleDragEnd(e) {
+    let el = e.currentTarget;
+    // console.log(":(")
+    el.style.top = "";
+    el.style.left = "";
+    el.classList.remove("draggin");
   }
 }
 
 
 // llamar a la libreria
-(function(){
-  new DragList("ul", "li");
+(function () {
+  new DragList("ul");
 })()
